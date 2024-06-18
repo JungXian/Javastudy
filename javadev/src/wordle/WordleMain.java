c2eb8dcf2579d970831836370c001d1685ed3e80
ukjin <jungukjin32@gmail.com>
2024-06-18 11:53:52 +0900

    첫 커밋 시작

---
+++ javadev/src/wordle/WordleMain.java
@@ -0,0 +1,24 @@
+package wordle;
+import java.util.Scanner;
+public class WordleMain {
+
+	public static void main(String[] args) {
+		//숫자야구 개념
+		//단어 리스트 생성
+		// 변수 : 입력할 단어, 정답 단어
+		String myWord1;
+		String myWord2;
+		String myWord3;
+		String myWord4;
+		String[] rWord = {"word","tree","code"};
+		String str = "hello";
+		char[] chara = new char[str.length()];
+		for(int i=0 ; i<str.length() ; i++)
+		{
+			chara[i] = str.charAt(i);
+			System.out.println(chara[i]);
+		}
+		//2차원배열
+	}
+
+}
